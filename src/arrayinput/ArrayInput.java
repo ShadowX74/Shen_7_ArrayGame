33ba5044952ec3192919f24d8859dad76c654f2d
Xiangyu Shen <xshen2013@gmail.com>
2016-05-04 16:06:01 -0500

    Beginning to start randomized larger map with walls.

--- src/arrayinput/ArrayInput.java
+++ src/arrayinput/ArrayInput.java
@@ -27,12 +27,13 @@ public class ArrayInput {
             if (answer.toLowerCase().contains("n")) {
                 play = false;
             }
+            notWinorLose = true;
         }
     }
     
     public static void game() {
         //creating array map
-        char[][] map = new char[21][21];
+        char[][] map = new char[501][501];
         int x = 10;
         int y = 10;
 
@@ -77,9 +78,27 @@ public class ArrayInput {
             map[x][y] = '@';
             map[enemyX][enemyY] = 'E';
             
-            for (int i = 0; i <= map[0].length - 1; i++) {
-                for (int j = 0; j <= map[1].length - 1; j++) {
-                    if (j < map[1].length - 1) {
+//            for (int i = 0; i <= map[0].length - 1; i++) {
+//                for (int j = 0; j <= map[1].length - 1; j++) {
+//                    if (j < map[1].length - 1) {
+//                        if (map[i][j] != '@' && map[i][j] != '*' && map[i][j] != 'T' && map[i][j] != 'E') {
+//                            System.out.print(". ");
+//                        } else {
+//                            System.out.print(map[i][j]  + " ");
+//                        }
+//                    } else {
+//                        if (map[i][j] != '@' && map[i][j] != '*' && map[i][j] != 'T' && map[i][j] != 'E') {
+//                            System.out.println(". ");
+//                        } else {
+//                            System.out.println(map[i][j] + " ");
+//                        }
+//                    }
+//                }
+//            }
+            
+            for (int i = 0; i <= map[0].length - 1 && i < x + 10 && i < x - 10; i++) {
+                for (int j = 0; j <= map[1].length - 1 && j < y + 10 && j < y - 10; j++) {
+                    if (j < map[1].length - 1 || j < y + 10) {
                         if (map[i][j] != '@' && map[i][j] != '*' && map[i][j] != 'T' && map[i][j] != 'E') {
                             System.out.print(". ");
                         } else {
@@ -94,7 +113,7 @@ public class ArrayInput {
                     }
                 }
             }
-            
+
             map[x][y] = '.';
             map[enemyX][enemyY] = '.';
             
@@ -123,7 +142,17 @@ public class ArrayInput {
             } else if (enemyY > y){
                 enemyY -= 1;
             }
-                        
+            
+            if (x >= 20) {
+                x = 0;
+            } else if (x <= 0) {
+                x = 20;
+            } else if (y >= 20) {
+                y = 0;
+            } else if (y <= 0) {
+                y = 20;
+            }
+            
             player = x + "," + y;
             enemy = enemyX + "," + enemyY;
             
